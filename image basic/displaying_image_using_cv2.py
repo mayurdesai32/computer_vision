83308b80714e747b6d46a9fefaf45ccbacd9d7f4
Mayur Sadashiv Desai <76611053+mayurdesai32@users.noreply.github.com>
2022-11-10 03:26:38 +0530

    image imageProcessing

---
+++ image basic/displaying_image_using_cv2.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread('../data/00-puppy.jpg')
+
+
+while True:
+    cv2.imshow('pic', img)
+# by pressing esc
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
